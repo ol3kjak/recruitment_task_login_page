a622cda7344f8a998d667a6b3d4098ed6ede7af1
Aleksander Jakubowski <ol3kjak@gmail.com>
2020-09-14 22:34:01 +0200

    - Added formilk, and configuret it, - Added yup, and configuret it, - Ended styling,
    TODO:
    - missing axios and configuration,
    - seperate components,
    - correct package versions in  package.json -> latest version shoulden't
    be used

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,125 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
-import {Button, Grid, Paper, TextField} from "@material-ui/core";
+import {Box, Button, Container, Grid, Paper, TextField, Typography} from "@material-ui/core";
+
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            padding: '5vh 0',
-            margin: theme.spacing(2),
-            textAlign: "center",
-            backgroundColor: "#fafafa",
+            display: 'block',
+            minWidth: 200,
+            padding: '5vh 0 2vh 0',
+            marginBottom: theme.spacing(3),
+            flexWrap: 'wrap',
         },
+        formClass: {
+            display: 'block',
+            backgroundColor: 'white ',
+            minWidth: '90%',
+        },
+        errorMessage: {
+            display: 'inline-block',
+            height: 15,
+            fontSize: 10,
+            color: "red",
+        },
+        buttonAdditionalClass: {
+            marginTop: 50,
+        }
     }),
 );
 
-
 const Login = () => {
     const classes = useStyles();
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ""
+        },
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .email('Invalid email address')
+                .required('Required'),
+            password: Yup.string()
+                .matches(/(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}/,
+                    {message: "Password must be minimum eight characters, at least one uppercase letter, one lowercase letter and one number"})
+                .required('Required'),
+        }),
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
     return (
         <Paper elevation={3} square={false} className={classes.root}>
             <Grid
                 container
-                spacing={2}
                 direction="row"
                 justify="center"
                 alignItems="center"
             >
                 <Grid item xs={10}>
-                    <Typography variant="h4" component="h1" gutterBottom>
+                    <Typography variant="h3" component="h1" gutterBottom align="center">
                         Login
                     </Typography>
                 </Grid>
-                <Grid item xs={10}>
-                    <TextField id="email" label="email" type="email" variant="outlined"/>
-                </Grid>
-                <Grid item xs={10}>
-                    <TextField id="password" label="password" type="password" variant="outlined"/>
-                </Grid>
-                <Grid item xs={10}>
-                    <Button
-                        color="primary"
-                        variant="contained"
-                        size="medium"
-                    >
-                        SUBMIT
-                    </Button>
-                </Grid>
+                <Container maxWidth="md">
+                    <form onSubmit={formik.handleSubmit} className={classes.formClass}>
+                        <Grid item xs={12}>
+                            <TextField
+                                id="email"
+                                label="Email"
+                                type="email"
+                                variant="outlined"
+                                margin="normal"
+                                fullWidth
+                                {...formik.getFieldProps('email')}
+                                error={Boolean(formik.touched.email && formik.errors.email)}
+                            />
+                            <span className={classes.errorMessage}>
+                                {formik.touched.email && formik.errors.email ? (
+                                    formik.errors.email) : ''}
+                            </span>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                id="password"
+                                label="Password"
+                                type="password"
+                                variant="outlined"
+                                margin="normal"
+                                fullWidth
+                                {...formik.getFieldProps('password')}
+                                error={Boolean(formik.touched.password && formik.errors.password)}
+                            />
+                            <span className={classes.errorMessage}>
+                                {formik.touched.password && formik.errors.password ? (
+                                    formik.errors.password) : null}
+                            </span>
+                        </Grid>
+                        <Grid container justify="center">
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                size="large"
+                                type="submit"
+                                className={classes.buttonAdditionalClass}
+                            >
+                                SUBMIT
+                            </Button>
+                        </Grid>
+                    </form>
+                </Container>
             </Grid>
         </Paper>
     );
 }
+
 
 export default function App() {
     return (
