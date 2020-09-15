98cb31e8ee6b92f8bd84c4241d74b5a9ece0f261
Aleksander Jakubowski <ol3kjak@gmail.com>
2020-09-15 10:28:52 +0200

    Updates

---
+++ precache-manifest.89dbf9f38ed09a478864b30eba854584.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd32e478db5c8b0dab195ef87229a2ca",
+    "url": "/recruitment_task_login_page/index.html"
+  },
+  {
+    "revision": "8d3c409166dfe5278453",
+    "url": "/recruitment_task_login_page/static/js/2.3aedb7a8.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/recruitment_task_login_page/static/js/2.3aedb7a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45143241acca1eaa97f",
+    "url": "/recruitment_task_login_page/static/js/main.0c5aeed1.chunk.js"
+  },
+  {
+    "revision": "976203c0c52626e7d708",
+    "url": "/recruitment_task_login_page/static/js/runtime-main.f8078d8b.js"
+  }
+]);
